AlesiaSherstneva/spring-boot-kafka-microservices

Add webservices for new topic

03-saga-pattern-app/orders-service/src/main/java/com/appsdeveloperblog/orders/service/OrderServiceImpl.java
@@ -1,0 +1,34 @@
+package com.appsdeveloperblog.orders.service;
+
+import com.appsdeveloperblog.core.dto.Order;
+import com.appsdeveloperblog.core.types.OrderStatus;
+import com.appsdeveloperblog.orders.dao.jpa.entity.OrderEntity;
+import com.appsdeveloperblog.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order placeOrder(Order order) {
+        OrderEntity entity = new OrderEntity();
+        entity.setCustomerId(order.getCustomerId());
+        entity.setProductId(order.getProductId());
+        entity.setProductQuantity(order.getProductQuantity());
+        entity.setStatus(OrderStatus.CREATED);
+        orderRepository.save(entity);
+
+        return new Order(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getProductId(),
+                entity.getProductQuantity(),
+                entity.getStatus());
+    }
+
+}

03-saga-pattern-app/payments-service/src/main/java/com/appsdeveloperblog/payments/dao/jpa/repository/PaymentRepository.java
@@ -1,0 +1,11 @@
+package com.appsdeveloperblog.payments.dao.jpa.repository;
+
+import com.appsdeveloperblog.payments.dao.jpa.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEntity, UUID> {
+}

03-saga-pattern-app/products-service/src/main/java/com/appsdeveloperblog/products/ProductsServiceApplication.java
@@ -1,0 +1,12 @@
+package com.appsdeveloperblog.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsServiceApplication.class, args);
+    }
+}

03-saga-pattern-app/products-service/src/main/java/com/appsdeveloperblog/products/dao/jpa/repository/ProductRepository.java
@@ -1,0 +1,11 @@
+package com.appsdeveloperblog.products.dao.jpa.repository;
+
+import com.appsdeveloperblog.products.dao.jpa.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+}

AlesiaSherstneva/event-driven-dev-with-kafka

03-saga-pattern-app/orders-service/src/main/java/com/develop/orders/service/OrderServiceImpl.java
@@ -1,0 +1,33 @@
+package com.develop.orders.service;
+
+import com.develop.core.dto.Order;
+import com.develop.core.types.OrderStatus;
+import com.develop.orders.dao.jpa.entity.OrderEntity;
+import com.develop.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order placeOrder(Order order) {
+        OrderEntity entity = new OrderEntity();
+        entity.setCustomerId(order.getCustomerId());
+        entity.setProductId(order.getProductId());
+        entity.setProductQuantity(order.getProductQuantity());
+        entity.setStatus(OrderStatus.CREATED);
+        orderRepository.save(entity);
+
+        return new Order(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getProductId(),
+                entity.getProductQuantity(),
+                entity.getStatus());
+    }
+}

03-saga-pattern-app/payments-service/src/main/java/com/develop/payments/dao/jpa/repository/PaymentRepository.java
@@ -1,0 +1,11 @@
+package com.develop.payments.dao.jpa.repository;
+
+import com.develop.payments.dao.jpa.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEntity, UUID> {
+}

03-saga-pattern-app/products-service/src/main/java/com/develop/products/ProductsServiceApplication.java
@@ -1,0 +1,11 @@
+package com.develop.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsServiceApplication.class, args);
+    }
+}

03-saga-pattern-app/products-service/src/main/java/com/develop/products/dao/jpa/repository/ProductRepository.java
@@ -1,0 +1,11 @@
+package com.develop.products.dao.jpa.repository;
+
+import com.develop.products.dao.jpa.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+}