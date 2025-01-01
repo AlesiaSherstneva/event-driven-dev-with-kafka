78120e81fabc754d2627410e8f6c045526940c6a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-01-01 22:13:38 +0300

    Create OrderCreatedEvent and send it into Kafka broker

--- 03-saga-pattern-app/orders-service/src/main/java/com/develop/orders/service/OrderServiceImpl.java
+++ 03-saga-pattern-app/orders-service/src/main/java/com/develop/orders/service/OrderServiceImpl.java
@@ -1,16 +1,23 @@
 package com.develop.orders.service;
 
 import com.develop.core.dto.Order;
+import com.develop.core.dto.events.OrderCreatedEvent;
 import com.develop.core.types.OrderStatus;
 import com.develop.orders.dao.jpa.entity.OrderEntity;
 import com.develop.orders.dao.jpa.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${order.events.topic.name}")
+    private String orderEventsTopicName;
 
     @Override
     public Order placeOrder(Order order) {
@@ -20,6 +27,14 @@ public class OrderServiceImpl implements OrderService {
         entity.setProductQuantity(order.getProductQuantity());
         entity.setStatus(OrderStatus.CREATED);
         orderRepository.save(entity);
+
+        OrderCreatedEvent placeOrder = OrderCreatedEvent.builder()
+                .orderId(entity.getId())
+                .customerId(entity.getCustomerId())
+                .productId(entity.getProductId())
+                .productQuantity(entity.getProductQuantity())
+                .build();
+        kafkaTemplate.send(orderEventsTopicName, placeOrder);
 
         return new Order(
                 entity.getId(),
