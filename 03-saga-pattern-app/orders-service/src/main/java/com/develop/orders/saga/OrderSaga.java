50f14cd6c6d3ee0d5552253b0902dc0b975f7fbb
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-01-03 05:34:11 +0300

    Create saga orchestration class

---
+++ 03-saga-pattern-app/orders-service/src/main/java/com/develop/orders/saga/OrderSaga.java
@@ -0,0 +1,16 @@
+package com.develop.orders.saga;
+
+import com.develop.core.dto.events.OrderCreatedEvent;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = "${order.events.topic.name}")
+public class OrderSaga {
+    @KafkaHandler
+    public void handleEvent(@Payload OrderCreatedEvent event) {
+
+    }
+}
