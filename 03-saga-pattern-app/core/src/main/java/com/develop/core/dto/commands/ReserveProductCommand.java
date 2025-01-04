90c3effd80d8a4897968c7851390fb0f6d146c69
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-01-04 05:49:11 +0300

    Create a command for reserving a product

---
+++ 03-saga-pattern-app/core/src/main/java/com/develop/core/dto/commands/ReserveProductCommand.java
@@ -0,0 +1,20 @@
+package com.develop.core.dto.commands;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReserveProductCommand {
+    private UUID productId;
+    private Integer productQuantity;
+    private UUID orderId;
+}
