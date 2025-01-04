48179edc179b6073f337137d0f830d3a0e8f2f55
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-01-04 05:57:59 +0300

    Send a reserve product command from saga

--- 03-saga-pattern-app/orders-service/src/main/java/com/develop/orders/saga/OrderSaga.java
+++ 03-saga-pattern-app/orders-service/src/main/java/com/develop/orders/saga/OrderSaga.java
@@ -1,16 +1,31 @@
 package com.develop.orders.saga;
 
+import com.develop.core.dto.commands.ReserveProductCommand;
 import com.develop.core.dto.events.OrderCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @KafkaListener(topics = "${order.events.topic.name}")
+@RequiredArgsConstructor
 public class OrderSaga {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${products.commands.topic.name}")
+    private final String productCommandTopicName;
+
     @KafkaHandler
     public void handleEvent(@Payload OrderCreatedEvent event) {
-
+        ReserveProductCommand command = ReserveProductCommand.builder()
+                .productId(event.getProductId())
+                .productQuantity(event.getProductQuantity())
+                .orderId(event.getOrderId())
+                .build();
+        kafkaTemplate.send(productCommandTopicName, command);
     }
 }
