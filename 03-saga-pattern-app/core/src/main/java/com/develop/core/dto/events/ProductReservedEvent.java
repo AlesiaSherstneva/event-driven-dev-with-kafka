6dcea86e9abc86675926d5969ad788d1d654983f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-01-12 05:30:04 +0300

    Create ProductReservedEvent and send it to Kafka

---
+++ 03-saga-pattern-app/core/src/main/java/com/develop/core/dto/events/ProductReservedEvent.java
@@ -0,0 +1,22 @@
+package com.develop.core.dto.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductReservedEvent {
+    private UUID orderId;
+    private UUID productId;
+    private BigDecimal productPrice;
+    private Integer productQuantity;
+}
--- 03-saga-pattern-app/products-service/src/main/java/com/develop/products/service/handler/ProductCommandsHandler.java
+++ 03-saga-pattern-app/products-service/src/main/java/com/develop/products/service/handler/ProductCommandsHandler.java
@@ -2,11 +2,14 @@ package com.develop.products.service.handler;
 
 import com.develop.core.dto.Product;
 import com.develop.core.dto.commands.ReserveProductCommand;
+import com.develop.core.dto.events.ProductReservedEvent;
 import com.develop.products.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +19,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ProductCommandsHandler {
     private final ProductService productService;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${products.events.topic.name}")
+    private String productsEventsTopicName;
 
     @KafkaHandler
     public void handleCommand(@Payload ReserveProductCommand command) {
@@ -24,8 +31,15 @@ public class ProductCommandsHandler {
                     .id(command.getProductId())
                     .quantity(command.getProductQuantity())
                     .build();
+            Product reservedProduct = productService.reserve(desiredProduct, command.getOrderId());
 
-            productService.reserve(desiredProduct, command.getOrderId());
+            ProductReservedEvent productReservedEvent = ProductReservedEvent.builder()
+                    .orderId(command.getOrderId())
+                    .productId(command.getProductId())
+                    .productPrice(reservedProduct.getPrice())
+                    .productQuantity(command.getProductQuantity())
+                    .build();
+            kafkaTemplate.send(productsEventsTopicName, productReservedEvent);
         } catch (Exception ex) {
             log.error(ex.getLocalizedMessage(), ex);
         }
