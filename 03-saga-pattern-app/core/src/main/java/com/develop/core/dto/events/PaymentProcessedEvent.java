c61c7dccecbd7c80f757b1cec6b4e3fd733895bd
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-01-23 13:01:35 +0300

    Create class for payment processed event

---
+++ 03-saga-pattern-app/core/src/main/java/com/develop/core/dto/events/PaymentProcessedEvent.java
@@ -0,0 +1,19 @@
+package com.develop.core.dto.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentProcessedEvent {
+    private UUID orderId;
+    private UUID paymentId;
+}
